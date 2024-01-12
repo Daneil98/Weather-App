2bf27ee64178d3a75df7dd5344ad8759fc1cb261
Daniel Idahosa <75020969+Daneil98@users.noreply.github.com>
2024-01-12 19:19:15 +0100

    Update weatherapi.py

--- View_Weather/weatherapi.py
+++ View_Weather/weatherapi.py
@@ -6,7 +6,7 @@ headers = {
     }
 
 def get_meta_data(location):
-    requestResponse = requests.get("http://api.weatherapi.com/v1/current.json?key=46723aaad5a94c3c961130530232408&q={}&aqi=no".format(location), headers= headers)
+    requestResponse = requests.get("http://api.weatherapi.com/v1/current.json?'your key'={}&aqi=no".format(location), headers= headers)
     return requestResponse.json()
     
 
